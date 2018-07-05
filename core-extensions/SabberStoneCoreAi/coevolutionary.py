04a82eb73ea8e5446ff0134e2f605fb9e59e5ef2
Pablo García Sánchez <fergunet@gmail.com>
2018-07-05 18:09:24 +0200

    Added the evaluation process

--- core-extensions/SabberStoneCoreAi/coevolutionary.py
+++ core-extensions/SabberStoneCoreAi/coevolutionary.py
@@ -1,8 +1,12 @@
 from random import Random
+from random import randint
 from time import time
 import inspyred
 #from inspyred.ec import Individual
 
+DEBUG = True
+DECKS = ["MAGE", "SHAMAN" , "WARRIOR"]
+NUM_GAMES = 10
 
 def generate_weights(random, args):
 	size = args.get('num_weights')
@@ -11,22 +15,64 @@ def generate_weights(random, args):
 
 def individual_to_commandline(ind):
 	#if ind is Individual get only the num_weights! else check that ind is a list of num_weights
-	return ""
+	param = ""
+	for e in ind:
+		param = param+str(e)+"#"
+	param = param[:-1]
+	return param
 
+def fight(f1, f2, d1, d2):
+	w = randint(0,10)
+	w1 = w
+	w2 = 10 - w
+	cml1 = individual_to_commandline(f1)
+	cml2 = individual_to_commandline(f2)
+
+	if DEBUG: print "\t\tdotnet run {0} {1} {2} {3} {4}".format(cml1,d1,cml2,d2,NUM_GAMES)
+	return w1, w2
 
 def evaluate_hearthstone(candidates, args):
 	parents = args['_ec'].population
+
+	to_fight = []
 	for p in parents:
-		print("FITNESS OF PARENT IS "+str(p.fitness))
-		p.fitness = 0
+		n = len(p.candidate) // 2 #removing the half (taus)
+		to_fight.append(p.candidate[:n])
+	num_parents = len(parents)
+
+	to_fight = to_fight + candidates
+	victories = []
+
 	fitness = []
-	for c in candidates:
-		print('Candidate {0} has a length of {1}'.format(c,len(c)))
-		total = 0
-		for i in range(0,len(c)):
-			total += c[i]
-			#print "Total is " +str(total)
-		fitness.append(total)
+
+	for i in range(0,len(to_fight)):
+		victories.append({})
+		victories[i]["TOTAL"] = 0
+		for d1 in DECKS:
+			for d2 in DECKS:
+				victories[i][d1+d2] = 0
+
+	for i,f1 in enumerate(to_fight):
+		if DEBUG: print "INDIVIDUAL "+str(f1)
+		for j,f2 in enumerate(to_fight):
+			for d1 in DECKS:
+				for d2 in DECKS:
+					if i<=j:
+						if DEBUG: print "\tVERSUS " + str(f2)
+						#if DEBUG: print ("\t\tCONFRONTING {0} vs {1} Ind{2} vs Ind{3}".format(d1,d2,f1,f2))
+						v1,v2 = fight(f1,f2,d1,d2)
+						victories[i]["TOTAL"]+= v1
+						victories[i][d1+d2] += v1
+						victories[j]["TOTAL"]+=v2
+						victories[j][d2+d1] += v2
+						#if DEBUG: print "\t\t"+str(v1)+" "+str(v2)
+	for i,v in enumerate(victories):
+		if i<num_parents:
+			print("Updating parent fitness")
+		else:
+			fitness.append(victories[i]["TOTAL"])
+
+
 	return fitness
 
 
@@ -42,10 +88,10 @@ def main(prng=None, display=False):
 	final_pop = ea.evolve(generator=generate_weights,
 						  num_weights = 2,
 						  evaluator=evaluate_hearthstone,
-						  pop_size=10,
+						  pop_size=5,
 						  bounder=inspyred.ec.Bounder(0,1),
 						  maximize=True,
-						  max_evaluations=100)
+						  max_evaluations=1000)
 
 	if display:
 		best = max(final_pop)
