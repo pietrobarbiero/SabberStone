a983b5a070b13041ce1c5afb38cb7502e1c9ccd7
Pablo García Sánchez <fergunet@gmail.com>
2018-11-12 17:49:01 +0100

    Added file to plot weights

---
+++ core-extensions/SabberStoneCoreAi/plotweights.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+import os
+
+directory = "/home/pgarcia/code/SabberStone/core-extensions/SabberStoneCoreAi/results/victories/"
+files = []
+
+for fname in os.listdir(directory):
+	if fname.startswith("inspyred-individuals"):
+		files.append(directory+fname)
+
+
+df_weights = pd.DataFrame(columns=( "HHA", "HAR", "BMHR", "BMAR", "BMA", "BMK", "BSR", "BMR", "MH", "MA", "MHC", "MHD", "MHDS", "MHI", "MHLS", "MHS", "MHT", "MHW", "MR", "MM", "MHP"))
+
+for f, filename in enumerate(files):
+	print "Reading file "+filename
+	with open(filename, 'rb') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			#print row
+			if row[0] == "99":
+				#print "LAST LINE"
+				#print row
+				values = []
+				for j in range(21):
+					v = row[j+3]
+					v = v.replace("[","")
+					v = float(v)
+					values.append(v)
+				#print values
+				df_weights.loc[len(df_weights)] = values
+
+print df_weights
+"""
+axes_battlefield = df_weights.boxplot(column=["HHA", "HAR", "BMHR", "BMAR", "BMA", "BMK", "BSR", "BMR"])
+axes_battlefield.set_ylabel("Weight")
+fig_battlefield = axes_battlefield.get_figure()
+fig_battlefield.savefig("weights_battlefield.png")
+"""
+axes_minions = df_weights.boxplot(column=["MH", "MA", "MHC", "MHD", "MHDS", "MHI", "MHLS", "MHS", "MHT", "MHW", "MHP", "MR", "MM"]) #MHP has changed the position!
+axes_minions.set_ylabel("Weight")
+fig_minions = axes_minions.get_figure()
+fig_minions.savefig("weights_minions.png")
+
+
